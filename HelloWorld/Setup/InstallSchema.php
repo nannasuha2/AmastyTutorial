c152550e88291b05acf48136b502bf8f5385bc95
NASUHA-PC\NASUHA <nannasuha2@gmail.com>
2018-10-31 07:28:56 -0700

    Change datatype for block_label into BLOB. Table does not automatically altered. Alter manually in mysql.

--- HelloWorld/Setup/InstallSchema.php
+++ HelloWorld/Setup/InstallSchema.php
@@ -29,7 +29,7 @@ class InstallSchema implements InstallSchemaInterface
             )
             ->addColumn(
                 'label',
-                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                \Magento\Framework\DB\Ddl\Table::TYPE_BLOB,
                 null,
                 ['default' => null, 'nullable' => false],
                 'Name'
