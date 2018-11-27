5473db9280acbd9353e32df0d561f5c0742b0866
NASUHA-PC\NASUHA <nannasuha2@gmail.com>
2018-11-27 13:56:27 +0800

    First test image upload. Image can be upload without error in admin page. Image shown in magento home page. "UploadImage" folder created in magento2 root folder. It store uploaded image.

--- HelloWorld/Model/Config/Backend/Image.php
+++ HelloWorld/Model/Config/Backend/Image.php
@@ -1,22 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: NASUHA
- * Date: 8/11/2018
- * Time: 3:50 PM
- */
 
 namespace Amasty\HelloWorld\Model\Config\Backend;
 
 use Amasty\HelloWorld\Helper\Data;
+//use Magento\Framework\Exception\FileSystemException;
 
 
 class Image extends \Magento\Config\Model\Config\Backend\Image
 {
-    /*public function _getUploadDir(Data $data)
-    {
+    const UPLOAD_DIR = 'uploadImages';
 
-    }*/
+    protected $_maxFileSize = 2048;
+
+    protected $base = 'http://localhost/magento2/uploadImages/';
+
+    protected $path;
+
+    protected $_PATH;
+
+    public function _setGetBlockLabel(Data $data)
+    {
+        $this->path = $data->getBlockLabel();
+    }
+
+    public function _getUploadDir()
+    {
+        $this->_PATH = '$this->base, $this->path';
+        //echo $this->_PATH;
+
+        //return $this->_mediaDirectory->getAbsolutePath($this->_appendScopeInfo(self::UPLOAD_DIR));
+        //return $this->_mediaDirectory->getAbsolutePat($this->_PATH);
+        //return "http://localhost/magento2/uploadImages/ProfilePics.jpg";
+        //return $data->getBlockLabel();
+       return $this->_PATH;
+    }
     protected function _getAllowedExtensions()
     {
         return ['jpg', 'jpeg', 'gif', 'png', 'svg'];
